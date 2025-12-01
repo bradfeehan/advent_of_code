ff65779a4f25ab0707ec5a15951330565e20c630
Brad Feehan <git@bradfeehan.com>
2025-12-01 21:33:41 +1100

    mix aoc.gen 1

---
+++ lib/year2025/day01.ex
@@ -0,0 +1,15 @@
+defmodule Year2025.Day01 do
+  @moduledoc """
+  Day 01 — Day 1: Secret Entrance
+  """
+
+  @spec part(1 | 2, String.t()) :: term()
+  def part(1, input), do: Year2025.Day01.Part1.solve(input)
+  def part(2, _input) do
+    raise "Part 2 is not yet unlocked"
+  end
+
+  def part(part, _input) do
+    raise ArgumentError, "unsupported part #{inspect(part)} for Year2025.Day01"
+  end
+end
---
+++ lib/year2025/day01/part1.ex
@@ -0,0 +1,10 @@
+defmodule Year2025.Day01.Part1 do
+  @moduledoc """
+  Part 1 — Day 1: Secret Entrance
+  """
+
+  @spec solve(String.t()) :: term()
+  def solve(_input) do
+    raise "Day 01 part 1 has not been implemented yet"
+  end
+end
