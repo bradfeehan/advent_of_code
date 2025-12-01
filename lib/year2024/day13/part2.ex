0451d466bc47e1114c590a943bac0dd8243f6a44
Brad Feehan <git@bradfeehan.com>
2025-12-01 23:15:32 +1100

    mix aoc.gen 13 --year 2024

---
+++ lib/year2024/day13/part2.ex
@@ -0,0 +1,10 @@
+defmodule Year2024.Day13.Part2 do
+  @moduledoc """
+  Part 2 — Day 13: Claw Contraption
+  """
+
+  @spec solve(String.t()) :: term()
+  def solve(_input) do
+    raise "Day 13 part 2 has not been implemented yet"
+  end
+end
